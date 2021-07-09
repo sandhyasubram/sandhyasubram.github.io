3a6da5d9aa3128c5067f55828a1333deebf922cb
Sandhya <subramaniyansandhya@gmail.com>
2021-07-10 00:55:30 +0530

    about

--- js/intro.js
+++ js/intro.js
@@ -1,7 +1,4 @@
 let pageHeight = window.innerHeight;
-setTimeout(function(){ 
-    $("html, body").animate({ scrollTop: pageHeight}, 1200); 
-}, 8000);
 $('.mouse').on('click', function(){ 
     $("html, body").animate({ scrollTop: pageHeight}, 1000);
 });
